81f42f0d9cd8a78ee1d6101c283ed558c6bc0c31
EndoOfficial <104819150+EndoOfficial@users.noreply.github.com>
2022-09-26 20:29:46 +1000

    V2
    Started on the AI scripts

---
+++ Assets/Kyrie's Team/Chris/scripts/State.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State
+{
+    public enum STATE
+    {
+        IDLE, ATTACK, PERSUIT
+    }
+    
+    public enum EVENT
+    {
+        ENTER, UPDATE, EXIT
+    }
+
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+    protected NavMeshAgent agent;
+
+    float visDist = 30f;
+    float visAngle = 360;
+    float attackDist = 5;
+
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        agent = _agent;
+        anim = _anim;
+        stage = EVENT.ENTER;
+        player = _player;
+    }
+
+    public virtual void Enter() { stage = EVENT.UPDATE; }
+    public virtual void Update() { stage = EVENT.UPDATE; }
+    public virtual void Exit() { stage = EVENT.EXIT; }
+
+    public State Process()
+    {
+        if (stage == EVENT.ENTER) Enter();
+        if (stage == EVENT.UPDATE) Update();
+        if (stage == EVENT.EXIT)
+        {
+            Exit();
+            return nextState;
+        }
+        return this;
+    }
+}
