8489d08ba77efff19355b737bf2932dc0a549dc3
zachary-coxsae <1026662@student.sae.edu.au>
2022-10-11 17:38:14 +1100

    V4.8
    Added Gun and Gun Animation

--- Assets/Scripts/Universal/Player/Gun.cs
+++ Assets/Scripts/Universal/Player/Gun.cs
@@ -6,23 +6,25 @@ public class Gun : MonoBehaviour
 {
     public int damage = 10;
     public Camera fpsCam;
-    public ParticleSystem muzzelFlash;
     public float impactForce = 30f;
     public float fireRate= 15f;
     private float nextTimeToFire = 0;
     public LayerMask EnemyLayer;
+    public Animator anim;
 
     public void Update()
     {
         if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
+            Debug.Log("here1");
             nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
     void Shoot()
     {
-        muzzelFlash.Play();
+        anim.SetTrigger("Shoot");
+        Debug.Log("here2");
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, EnemyLayer))
