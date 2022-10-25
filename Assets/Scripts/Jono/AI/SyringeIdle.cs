2fce3bf52e2d5b7e4e6ebefcbcc90d877e1b58d4
zachary-coxsae <1026662@student.sae.edu.au>
2022-10-25 19:53:37 +1100

    v6.18
    syringe change

--- Assets/Scripts/Jono/AI/SyringeIdle.cs
+++ Assets/Scripts/Jono/AI/SyringeIdle.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.AI;
+//using UnityEditor.AI; << wrong move buccko
+using UnityEngine.AI;
 
 public class SyringeIdle : SyringeState
 {
