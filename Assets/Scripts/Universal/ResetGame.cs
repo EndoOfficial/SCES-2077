55a4b17404e343fbc17ba487966520c2db3a2ebd
zachary-coxsae <1026662@student.sae.edu.au>
2022-11-01 16:14:35 +1100

    v7.14
    main menu

---
+++ Assets/Scripts/Universal/ResetGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetGame : MonoBehaviour
+{
+  
+    public void ResetPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
