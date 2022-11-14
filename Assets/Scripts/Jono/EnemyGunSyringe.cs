9fc1b0fccda92b2205bfca85205a9589cbb6faf0
EndoOfficial <104819150+EndoOfficial@users.noreply.github.com>
2022-11-14 16:45:05 +1100

    v9.1
    double checked an finalized the animations for Chris and Jono
    added enemy trails
    fixed trail error
    made EnemyGun have protected variables

--- Assets/Scripts/Jono/EnemyGunSyringe.cs
+++ Assets/Scripts/Jono/EnemyGunSyringe.cs
@@ -2,137 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyGunSyringe : MonoBehaviour
+public class EnemyGunSyringe : EnemyGun
 {
-    public GameObject player;
-    private Animator anim;
-
-    private bool canSeePlayerRay;
-    private bool shootAtPlayerRay;
-    public bool seePlayer;
-    private Vector3 playerDirection;
-    private Vector3 OldPlayerDirection;
-    private Vector3 bulletDirection;
-    public LayerMask playerMask;
-    private float bulletSpread = 0.01f;
-    public float maxSpread;
-    private float tempSpread;
-    public float RateOfFire;
-    public int damage;
     public float PositionDelay;
-    public bool Paused;
     public GroundCheck grounded;
-    private void Start()
-    {
-        player = GameObject.Find("Player");
-        anim = GetComponent<Animator>();
-        tempSpread = bulletSpread;
-        StartCoroutine(Position());
 
-        StartCoroutine(Shoot());
-    }
-   
-
-    private void FixedUpdate()
-    {
-        //gets player direction
-
-        playerDirection = player.transform.position - transform.position;
-
-        //playerDirection = playerDirection;
-
-        //if enemy can see player
-        if (canSeePlayerRay = Physics.Raycast(transform.position, playerDirection, out RaycastHit hitinfo))
-        {
-            
-            if (hitinfo.transform.CompareTag("Player") && !seePlayer)
-            {
-                seePlayer = true;                
-            }
-            else if (!hitinfo.transform.CompareTag("Player") && seePlayer)
-            {
-                seePlayer = false;
-                bulletSpread = tempSpread;                         
-            }
-        }
-    }
-
-    private void OnEnable()
-    {
-        GameEvents.OnPauseGame += PauseGame;
-    }
-    private void OnDisable()
-    {
-        GameEvents.OnPauseGame -= PauseGame;
-    }
-    // Update is called once per frame
-    void PauseGame(bool paused)
-    {
-        Paused = paused;
-    }
     //shoot coroutine
-    private IEnumerator Shoot()
+    protected override IEnumerator Shoot()
     {
-        yield return new WaitForSecondsRealtime(1f);
-        while (true)
+        if (!grounded.Grounded && seePlayer && !Paused)
         {
-            //if (!seePlayer)
-            //{
-            //    yield return null;
-            //}
-            //if (Paused)
-            //{
-            //    yield return null;
+            //randomizes bulletSpread
+            float temp = Random.Range(-bulletSpread, bulletSpread);
+            float temp1 = Random.Range(-bulletSpread, bulletSpread);
+            float temp2 = Random.Range(-bulletSpread, bulletSpread);
 
-            //}
-            if (!grounded.Grounded && seePlayer && !Paused)
+            //get the normalized playerDirection
+            bulletDirection = OldPlayerDirection;
+            //applies the random bulletspread to the bullet direction
+            bulletDirection = new Vector3(
+                bulletDirection.x + temp,
+                bulletDirection.y + temp1,
+                bulletDirection.z + temp2);
+
+            //if the ray hits the player
+            if (shootAtPlayerRay = Physics.Raycast(transform.position, bulletDirection, out RaycastHit hit))
             {
-                //randomizes bulletSpread
-                float temp = Random.Range(-bulletSpread, bulletSpread);
-                float temp1 = Random.Range(-bulletSpread, bulletSpread);
-                float temp2 = Random.Range(-bulletSpread, bulletSpread);
-
-                //get the normalized playerDirection
-                bulletDirection = OldPlayerDirection;
-                //applies the random bulletspread to the bullet direction
-                bulletDirection = new Vector3(
-                    bulletDirection.x + temp,
-                    bulletDirection.y + temp1,
-                    bulletDirection.z + temp2);
-
-                //if the ray hits the player
-                if (shootAtPlayerRay = Physics.Raycast(transform.position, bulletDirection, out RaycastHit hitinfo))
-                {
-                    anim.SetTrigger("Shoot");
-                    if (hitinfo.transform.CompareTag("Player"))
-                    {
-                        GameEvents.DamagePlayer?.Invoke(damage);
-                    }
-                }
-                //increases spread every shot
-                if (bulletSpread <= maxSpread)
-                {
-                    bulletSpread += 0.01f;
-                }
+                anim.SetTrigger("Shoot");
+                TrailRenderer trail = Instantiate(bulletTrail, transform.position, Quaternion.identity);
+                base.StartCoroutine(SpawnTrail(trail, hit));
             }
-            yield return new WaitForSecondsRealtime(RateOfFire);
+            //increases spread every shot
+            if (bulletSpread <= maxSpread)
+            {
+                bulletSpread += 0.01f;
+            }
         }
-
-    }
-    public IEnumerator Position()
-    {
-        yield return new WaitForSecondsRealtime(PositionDelay);
-        OldPlayerDirection = playerDirection;
-        StartCoroutine(Position());
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position, bulletDirection);
-        Gizmos.color = Color.green;
-        Gizmos.DrawRay(transform.position, playerDirection);
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, OldPlayerDirection);
+        yield return new WaitForSecondsRealtime(attackSpeed);
     }
 }
