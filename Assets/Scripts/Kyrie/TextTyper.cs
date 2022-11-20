4bd0259416e5b9f7aa53efeafa185898c4588019
Nihil16 <102133258+Nihil16@users.noreply.github.com>
2022-11-21 07:04:45 +1100

    V9.27
    +Cat Level
    +Meow

--- Assets/Scripts/Kyrie/TextTyper.cs
+++ Assets/Scripts/Kyrie/TextTyper.cs
@@ -7,22 +7,24 @@ using TMPro;
 
 public class TextTyper : MonoBehaviour
 {
+    // Is the text
     [SerializeField] TextMeshProUGUI _textMeshPro;
 
+    //is the array all the characters 
     public string[] stringArray;
-
-    [SerializeField] float timeBtwnChars;
-    private float timeBtwnWords;
-
+    //is the count FOR the array
     int i = 0;
 
     void Start()
     {
+        //starts function on frame 1 
         EndCheck();
     }
 
     public void EndCheck()
     {
+        //if the array lerngth is higher then ARRAY -1 by away and start coroutine
+        //this will happen for each string of the array
         if (i <= stringArray.Length - 1)
         {
             _textMeshPro.text = stringArray[i];
@@ -32,26 +34,29 @@ public class TextTyper : MonoBehaviour
 
     private IEnumerator TextVisible()
     {
+        //makes the text update
         _textMeshPro.ForceMeshUpdate();
-        int totalVisibleCharacters = _textMeshPro.textInfo.characterCount;
+        //Caculates the amount of character count that is needed for it to finish
+        int totalVisibleCharacters = _textMeshPro.textInfo.characterCount; 
+        //count is 0 and will go alway to the max of the array and will be stopped by Endcheck count
         int counter = 0;
 
         while (true)
         {
-            int visibleCount = counter % (totalVisibleCharacters + 1);
+            //Using the already built in MaxVisibleChararacters we are allowed to show the first i amount of character
+            int visibleCount = counter;
             _textMeshPro.maxVisibleCharacters = visibleCount;
 
+            //simple count to allow us to go up by each letter, change the 1 to change how many letters to show.
             if (visibleCount >= totalVisibleCharacters)
             {
                 i += 1;
-                Invoke("EndCheck", timeBtwnWords);
-                break;
             }
 
             counter += 1;
-            yield return new WaitForSeconds(timeBtwnChars);
-
-
+            yield return new WaitForSeconds(0f);
+            //chang the 0 to change how slow it is 0.02 could work
         }
+        
     }
 }
