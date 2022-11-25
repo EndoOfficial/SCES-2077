85deaa78c711638055e47f8fdcfe5a63363918f8
Nihil16 <102133258+Nihil16@users.noreply.github.com>
2022-11-26 10:25:27 +1100

    v10.21
    Added Menu Area Change
    Corpo still needs to be added.
    To tired.

--- Assets/Scripts/Kyrie/CameraSwap.cs
+++ Assets/Scripts/Kyrie/CameraSwap.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class CameraSwap : MonoBehaviour
 {
-    public Camera MainCamera;
+    int i = 2;
+    public Camera MainCamera1;
+    public Camera MainCamera2;
+    public Camera MainCamera3;
+    public Camera MainCamera4;
+
     public Camera OptionsCamera;
     public Camera CreditCamera;
     public GameObject MainCanvas;
@@ -12,20 +17,20 @@ public class CameraSwap : MonoBehaviour
     public GameObject CreditCanvas;
     public void Start()
     {
+        
         MainCanvas.SetActive(true);
         OptionsCanvas.SetActive(false);
         CreditCanvas.SetActive(false);
-        MainCamera.enabled = true;
+        RandomArea();
         OptionsCamera.enabled = false;
         CreditCamera.enabled = false;
     }
     public void SwaptoOptions()
         {
 
-            MainCamera.enabled = false;
             OptionsCamera.enabled = true;
             CreditCamera.enabled = false;
-            MainCanvas.SetActive(false);
+            TurnOffAreas();
             OptionsCanvas.SetActive(true);
             CreditCanvas.SetActive(false);
 
@@ -33,7 +38,7 @@ public class CameraSwap : MonoBehaviour
         public void SwaptoMain()
         {
 
-        MainCamera.enabled = true;
+        RandomArea();
         OptionsCamera.enabled = false;
         CreditCamera.enabled = false;
         MainCanvas.SetActive(true);
@@ -43,14 +48,60 @@ public class CameraSwap : MonoBehaviour
         public void SwaptoCredits()
         {
 
-        MainCamera.enabled = false;
+        TurnOffAreas();
         OptionsCamera.enabled = false;
         CreditCamera.enabled = true;
-        MainCanvas.SetActive(false);
+        TurnOffAreas();
         OptionsCanvas.SetActive(false);
         CreditCanvas.SetActive(true);
     }
 
 
+    public void RandomArea()
+    {
+        i = Random.Range(1, 5);
+        Debug.Log(i);
+        if (i == 1)
+        {
+            MainCamera1.enabled = true;
+            MainCamera2.enabled = false;
+            MainCamera3.enabled = false;
+            MainCamera4.enabled = false;
+        }
+        if (i == 2)
+        {
+            MainCamera1.enabled = false;
+            MainCamera2.enabled = true;
+            MainCamera3.enabled = false;
+            MainCamera4.enabled = false;
+        }
+        if (i == 3)
+        {
+            MainCamera1.enabled = false;
+            MainCamera2.enabled = false;
+            MainCamera3.enabled = true;
+            MainCamera4.enabled = false;
+        }
+        if (i == 4)
+        {
+             MainCamera1.enabled = false;
+             MainCamera2.enabled = false;
+             MainCamera3.enabled = false;
+             MainCamera4.enabled = true;
+        }
+
+
+
+    }
+    
+    public void TurnOffAreas()
+    {
+        MainCamera1.enabled = false;
+        MainCamera2.enabled = false;
+        MainCamera3.enabled = false;
+        MainCamera4.enabled = false;
+        MainCanvas.SetActive(false);
+    }
+       
 
 }
