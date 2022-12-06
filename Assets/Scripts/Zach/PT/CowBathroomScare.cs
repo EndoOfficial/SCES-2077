daf05e348635d31eb5445e2ed5071b85b996e2cc
zachary-coxsae <1026662@student.sae.edu.au>
2022-12-06 12:40:05 +1100

    v12.13
    pt jumpscare

--- Assets/Scripts/Zach/PT/CowBathroomScare.cs
+++ Assets/Scripts/Zach/PT/CowBathroomScare.cs
@@ -7,13 +7,26 @@ public class CowBathroomScare : MonoBehaviour
     public Transform endPoint;
     private float scareTime = 1;
     public GameObject Cow;
+    public GameObject door;
+    private AudioSource source;
 
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
+        GameEvents.OnRuralplayAudio?.Invoke(source, AudioManager.RuralClipTags.Moo);
         LeanTween.move(Cow, endPoint, scareTime)
-            .setEaseInOutQuart();
-        //play audio
-        //slam door
+            .setEaseInOutQuart()
+            .setOnComplete(() =>
+            {
+                //slam door
+                LeanTween.rotateAround(door, Vector3.up, -20, 0.5f);
+                //play audio
+                GameEvents.OnRuralplayAudio?.Invoke(source, AudioManager.RuralClipTags.DoorSlam);
+                Cow.SetActive(false);
+            });
     }
 
 }
